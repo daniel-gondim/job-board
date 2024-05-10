049ed19b30f25853b646aefe72d349a4f6ab2c9b
Daniel <danielgondimdecastro@gmail.com>
2024-05-10 20:03:46 -0300

    fix: adiciona a anotação Data e o atributo id à classe model

--- src/main/java/com/ads/gynvagas/jobboard/model/Vaga.java
+++ src/main/java/com/ads/gynvagas/jobboard/model/Vaga.java
@@ -1,9 +1,13 @@
 package com.ads.gynvagas.jobboard.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Vaga {
+    @Id
     private String nome;
     private String descricao;
     private String empresa;
