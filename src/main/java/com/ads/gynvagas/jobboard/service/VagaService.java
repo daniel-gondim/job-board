694d2304e2e832eeb6fa150291d9404841388f93
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-05-11 16:19:39 -0300

    feat: Adiciona serviço para manipulação de vagas
    Adiciona a classe VagaService responsável por intermediar a interação entre a camada de controle e o repositório de vagasEste serviço oferece métodos para adicionar uma nova vaga e para obter todas as vagas existentes no sistema

--- src/main/java/com/ads/gynvagas/jobboard/service/VagaService.java
+++ src/main/java/com/ads/gynvagas/jobboard/service/VagaService.java
@@ -1,4 +1,23 @@
 package com.ads.gynvagas.jobboard.service;
 
+import com.ads.gynvagas.jobboard.model.Vaga;
+import com.ads.gynvagas.jobboard.repository.VagaRepository;
+
+import java.util.List;
+
 public class VagaService {
+
+    private VagaRepository vagaRepository;
+
+    public VagaService(VagaRepository vagaRepository) {
+        this.vagaRepository = vagaRepository;
+    }
+
+    public Vaga adicionarVaga(Vaga vaga) {
+        return vagaRepository.save(vaga);
+    }
+
+    public List<Vaga> obterTodasAsVagas() {
+        return vagaRepository.findAll();
+    }
 }
