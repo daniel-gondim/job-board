ebfdb1f7480fec3906594402bb7e87b66d206f83
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-05-11 19:13:51 -0300

    feat: adiciona método editarVagas()

--- src/main/java/com/ads/gynvagas/jobboard/service/VagaService.java
+++ src/main/java/com/ads/gynvagas/jobboard/service/VagaService.java
@@ -2,9 +2,11 @@ package com.ads.gynvagas.jobboard.service;
 
 import com.ads.gynvagas.jobboard.model.Vaga;
 import com.ads.gynvagas.jobboard.repository.VagaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class VagaService {
 
     private VagaRepository vagaRepository;
@@ -20,4 +22,9 @@ public class VagaService {
     public List<Vaga> obterTodasAsVagas() {
         return vagaRepository.findAll();
     }
+
+    public Vaga editarVaga(int id, Vaga vaga) {
+        return vagaRepository.save(vaga);
+    }
+
 }
