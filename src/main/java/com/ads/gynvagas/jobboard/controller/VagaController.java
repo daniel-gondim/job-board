fe0a82e76598aa64badfa858405fba037717b0b2
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-05-12 19:51:19 -0300

    feat: implementa o controller VagaController e os métodos POST, PUT, DELETE e GET

--- src/main/java/com/ads/gynvagas/jobboard/controller/VagaController.java
+++ src/main/java/com/ads/gynvagas/jobboard/controller/VagaController.java
@@ -1,4 +1,40 @@
 package com.ads.gynvagas.jobboard.controller;
 
+import com.ads.gynvagas.jobboard.model.Vaga;
+import com.ads.gynvagas.jobboard.service.VagaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
 public class VagaController {
+
+    @Autowired
+    VagaService vagaService;
+
+    @GetMapping("/vagas")
+    public List<Vaga> obterTodasAsVagas () {
+        return vagaService.obterTodasAsVagas();
+    }
+
+    @PostMapping("/vagas")
+    public Vaga adicionarVaga(@RequestBody Vaga vaga) {
+        return vagaService.adicionarVaga(vaga);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Vaga> editarVaga(@PathVariable Integer id, @RequestBody Vaga vaga) {
+        Vaga vagaAtualizada = vagaService.editarVagas(id, vaga);
+        return ResponseEntity.ok(vagaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> removerVaga(@PathVariable Integer id) {
+        vagaService.deletaVaga(id);
+        return ResponseEntity.ok("Vaga deleteda com sucesso.");
+    }
+
 }
