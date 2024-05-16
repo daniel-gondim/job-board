223b036c46d728ec37751fc7c9111d287fc48d94
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-05-16 20:15:41 -0300

    test: adicionar e melhorar testes unitários para métodos de VagaService
    - adicionar testes para verificar a adição bem-sucedida de uma vaga com todos os campos preenchidos.
    - melhorar testes para obter todas as vagas, incluindo verificar propriedades específicas das vagas.
    - adicionar testes para lidar com exceções ao adicionar uma vaga.
    - adicionar testes para verificar o retorno de uma lista vazia ao obter todas as vagas

--- src/test/java/com/ads/gynvagas/jobboard/service/VagaServiceTest.java
+++ src/test/java/com/ads/gynvagas/jobboard/service/VagaServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,11 +19,28 @@ class VagaServiceTest {
         VagaService vagaService = new VagaService(mockRepository);
 
         Vaga vaga = new Vaga();
+        vaga.setNome("Desenvolvedor Java");
+        vaga.setDescricao("Desenvolvimento de aplicações Java");
+        vaga.setEmpresa("Empresa XYZ");
+        vaga.setContato("contato@empresa.com");
+        vaga.setSalario(5000.0);
+
         Mockito.when(mockRepository.save(vaga)).thenReturn(vaga);
 
         Vaga vagaSalva = vagaService.adicionarVaga(vaga);
 
-        assertEquals(vaga, vagaSalva);
+        vaga.setNome("Desenvolvedor Java");
+        vaga.setDescricao("Desenvolvimento de aplicações Java");
+        vaga.setEmpresa("Empresa XYZ");
+        vaga.setContato("contato@empresa.com");
+        vaga.setSalario(5000.0);
+
+        assertNotNull(vagaSalva);
+        assertEquals(vaga.getNome(), vagaSalva.getNome());
+        assertEquals(vaga.getDescricao(), vagaSalva.getDescricao());
+        assertEquals(vaga.getEmpresa(), vagaSalva.getEmpresa());
+        assertEquals(vaga.getContato(), vagaSalva.getContato());
+        assertEquals(vaga.getSalario(), vagaSalva.getSalario());
     }
 
     @Test
@@ -34,25 +50,34 @@ class VagaServiceTest {
 
 
         Vaga vaga1 = new Vaga();
+        vaga1.setNome("Desenvolvedor Java");
+        vaga1.setDescricao("Desenvolvimento de aplicações Java");
+        vaga1.setEmpresa("Empresa XYZ");
+        vaga1.setContato("contato@empresa.com");
+        vaga1.setSalario(5000.0);
+
         Vaga vaga2 = new Vaga();
-        Vaga vaga3 = new Vaga();
+        vaga2.setNome("Desenvolvedor Front-end");
+        vaga2.setDescricao("Desenvolvimento de interfaces de usuário");
+        vaga2.setEmpresa("Empresa ABC");
+        vaga2.setContato("contato@empresaabc.com");
+        vaga2.setSalario(4500.0);
 
         Mockito.when(mockRepository.save(vaga1)).thenReturn(vaga1);
         Mockito.when(mockRepository.save(vaga2)).thenReturn(vaga2);
-        Mockito.when(mockRepository.save(vaga3)).thenReturn(vaga3);
 
         vagaService.adicionarVaga(vaga1);
         vagaService.adicionarVaga(vaga2);
-        vagaService.adicionarVaga(vaga3);
 
         List<Vaga> listaDeVagas = new ArrayList<>();
+        Mockito.when(mockRepository.findAll()).thenReturn(listaDeVagas);
         listaDeVagas.add(vaga1);
-        listaDeVagas.add(vaga1);
-        listaDeVagas.add(vaga1);
+        listaDeVagas.add(vaga2);
 
-        assertEquals(true, listaDeVagas.contains(vaga1));
-        assertEquals(true, listaDeVagas.contains(vaga2));
-        assertEquals(true, listaDeVagas.contains(vaga3));
+        assertNotNull(listaDeVagas);
+        assertEquals(2, listaDeVagas.size());
+        assertTrue(listaDeVagas.contains(vaga1));
+        assertTrue(listaDeVagas.contains(vaga2));
     }
 
     @Test
@@ -74,5 +99,6 @@ class VagaServiceTest {
         List<Vaga> listaDeVagas = vagaService.obterTodasAsVagas();
 
         assertTrue(listaDeVagas.isEmpty());
+        assertNotNull(listaDeVagas);
     }
 }
