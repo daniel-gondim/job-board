b052a6046a617a456007b65543c64a02345b50d6
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-06-09 14:04:54 -0300

    fix: adiciona logs e corrige mapeamento de cors

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,5 +1,7 @@
 package config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,13 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Permite requisições apenas do localhost:4200
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        logger.info("Configuring CORS mappings");
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
-                .allowCredentials(true); // Headers permitidos
+                .allowCredentials(true);
     }
 }
 
